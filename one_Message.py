0ab124825257b4d99d90fcc421a04efcd8da65fa
Metalmerge <ermakovd06@gmail.com>
2023-11-16 19:16:46 -0500

    Fix file paths and error handling in script

--- one_Message.py
+++ one_Message.py
@@ -4,32 +4,40 @@ import time
 import random
 import configparser
 
-# random_delay = 1
-random_delay = random.randint(0, 60)
-print(f"Delaying for {random_delay} seconds")
+random_delay = random.randint(0, 10)
+print("Delaying for {} seconds".format(random_delay))
 time.sleep(random_delay)
 
-file_path = "text.txt"
+file_path = "/Users/dimaermakov/RATM/text.txt"
 
-# Load the configuration file
-config = configparser.ConfigParser()
-config.read("config.ini")
+# Ensure the path to the config file is correct
+config_path = "/Users/dimaermakov/RATM/config.ini"
 
-# Retrieve the phone number from the configuration file
-try:
-    number = config.get("Credentials", "PRIVATE_PHONE_NUMBER")
-except configparser.Error:
-    print("Error: Unable to read the configuration file.")
+# Check if the config file exists
+if not os.path.exists(config_path):
+    print("Error: Configuration file 'config.ini' not found.")
     sys.exit(1)
 
-script_path = "sendMessage.scpt"
+# Attempt to load the configuration file
+config = configparser.ConfigParser()
+try:
+    config.read(config_path)
+    number = config.get("Credentials", "PRIVATE_PHONE_NUMBER")
+except (configparser.Error, KeyError) as e:
+    print("Error: Unable to read the configuration file:", e)
+    sys.exit(1)
+
+script_path = "/Users/dimaermakov/RATM/sendMessage.scpt"
 
 with open(file_path, "r") as file:
     lines = file.readlines()
 
-# Select a random line
+# Check if the text file is empty
+if not lines:
+    print("Error: Text file 'text.txt' is empty.")
+    sys.exit(1)
+
 random_line = random.choice(lines).strip()
 
-# Send the selected random line
 command = f'osascript {script_path} {number} "{random_line}"'
 os.system(command)
